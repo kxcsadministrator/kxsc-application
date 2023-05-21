54829a929dfbf025545e859faa322f897aab7e60
CodeMaestro1000 <timothyakoji@gmail.com>
2023-05-21 18:39:52 +0100

    Fixed URL Bug

--- project/src/url.js
+++ project/src/url.js
@@ -1,5 +1,5 @@
 const API_URL = {
-  user: process.env.USER_SERVICE || "http://35.180.192.217:3000",
+  user: process.env.USERS_SERVICE || "http://35.180.192.217:3000",
   institute: process.env.INSTITUTE_SERVICE || "http://35.180.192.217:3001",
   resource: process.env.RESOURCES_SERVICE || "http://35.180.192.217:3002",
 };
