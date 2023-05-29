40b2f797968d7bab7f0e566ab6b04611c13d80a9
CodeMaestro1000 <timothyakoji@gmail.com>
2023-05-29 15:30:07 +0100

    Added docker compose for backend stack

--- project/src/url.js
+++ project/src/url.js
@@ -1,6 +1,6 @@
 const API_URL = {
-  user: process.env.USERS_SERVICE || "http://35.180.192.217:3000",
-  institute: process.env.INSTITUTE_SERVICE || "http://35.180.192.217:3001",
-  resource: process.env.RESOURCES_SERVICE || "http://35.180.192.217:3002",
+  user: process.env.USERS_SERVICE || "http://13.38.61.166:3002",
+  institute: process.env.INSTITUTE_SERVICE || "http://13.38.61.166:3001",
+  resource: process.env.RESOURCES_SERVICE || "http://13.38.61.166:3000",
 };
 export default API_URL;
