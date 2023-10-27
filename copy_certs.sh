265e29b18847136f7af286f9d718553b3b1f42db
CodeMaestro1000 <timothyakoji@gmail.com>
2023-10-27 06:19:14 +0100

    Added scrips to copy certificates

---
+++ copy_certs.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+sudo docker-compose down
+
+sudo cp /etc/letsencrypt/live/sedaltd.com/privkey.pem research_institutes/privkey.pem
+sudo cp /etc/letsencrypt/live/sedaltd.com/fullchain.pem research_institutes/fullchain.pem
+
+sudo cp /etc/letsencrypt/live/sedaltd.com/fullchain.pem research_resources/fullchain.pem
+sudo cp /etc/letsencrypt/live/sedaltd.com/privkey.pem research_resources/privkey.pem
+
+sudo cp /etc/letsencrypt/live/sedaltd.com/fullchain.pem users/fullchain.pem
+sudo cp /etc/letsencrypt/live/sedaltd.com/privkey.pem users/privkey.pem
+
+sudo docker-compose -f docker-compose-back.yaml up -d
