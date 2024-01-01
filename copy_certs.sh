37dd75564fe517312b802d59c992fabb9a7ebea4
kxcsadministrator <154379644+kxcsadministrator@users.noreply.github.com>
2024-01-01 13:38:41 +0000

    Update shell startup script

--- copy_certs.sh
+++ copy_certs.sh
@@ -3,13 +3,13 @@ cd knowledge-exchange-deployment/
 
 sudo docker-compose down
 
-sudo cp /etc/letsencrypt/live/sedaltd.com/privkey.pem research_institutes/privkey.pem
-sudo cp /etc/letsencrypt/live/sedaltd.com/fullchain.pem research_institutes/fullchain.pem
+sudo cp /etc/letsencrypt/live/moniat60.com.ng/privkey.pem research_institutes/privkey.pem
+sudo cp /etc/letsencrypt/live/moniat60.com.ng/fullchain.pem research_institutes/fullchain.pem
 
-sudo cp /etc/letsencrypt/live/sedaltd.com/fullchain.pem research_resources/fullchain.pem
-sudo cp /etc/letsencrypt/live/sedaltd.com/privkey.pem research_resources/privkey.pem
+sudo cp /etc/letsencrypt/live/moniat60.com.ng/fullchain.pem research_resources/fullchain.pem
+sudo cp /etc/letsencrypt/live/moniat60.com.ng/privkey.pem research_resources/privkey.pem
 
-sudo cp /etc/letsencrypt/live/sedaltd.com/fullchain.pem users/fullchain.pem
-sudo cp /etc/letsencrypt/live/sedaltd.com/privkey.pem users/privkey.pem
+sudo cp /etc/letsencrypt/live/moniat60.com.ng/fullchain.pem users/fullchain.pem
+sudo cp /etc/letsencrypt/live/moniat60.com.ng/privkey.pem users/privkey.pem
 
 sudo docker-compose -f docker-compose-back.yaml up -d
