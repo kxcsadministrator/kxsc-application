162ba446912d798b427f5481f1b02689ac581d7c
timothyakoji <timothyakoji@gmail.com>
2024-01-14 23:03:18 +0100

    Removed FE from project

--- project/src/url.js
+++
@@ -1,6 +0,0 @@
-const API_URL = {
-  user: process.env.USERS_SERVICE || "https://moniat60.com.ng:3000",
-  institute: process.env.INSTITUTE_SERVICE || "https://moniat60.com.ng:3001",
-  resource: process.env.RESOURCES_SERVICE || "https://moniat60.com.ng:3002",
-};
-export default API_URL;
